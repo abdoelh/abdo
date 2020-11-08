b99a27ac6ead9a00cdc0e1f074a4b35f370fa30f
Abdelmounaim Elhaila <abdelmounaimelhaila@gmail.com>
2020-11-08 23:09:07 +0100

    fix

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,12 @@
 var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 if (isMobile) {
-document.documentElement.webkitRequestFullScreen();
-document.documentElement.requestFullscreen();
-document.documentElement.msRequestFullscreen();
-
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.webkitRequestFullscreen) { /* Safari */
+    document.documentElement.webkitRequestFullscreen();
+  } else if (document.documentElement.msRequestFullscreen) { /* IE11 */
+    document.documentElement.msRequestFullscreen();
+  }
 }
 var c=0;
 function shownav(){
